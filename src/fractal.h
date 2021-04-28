003f2e01fa07a9dbcf343484a26c8f32612c7c4d
seudonym <80459261+seudonym@users.noreply.github.com>
2021-04-28 17:34:51 +0530

    Delete src directory

--- src/fractal.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-#include <iostream>
-#include <fstream>
-#include <cmath>
-
-const int ITERATIONS = 100;
-double map(double input, double iRange_start, double iRange_end, double oRange_start, double oRange_end);
-int Mandelbrot(double c_real, double c_imaginary);
